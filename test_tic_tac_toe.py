0d601578bf15086f3e53ddc91932bbe490c79f9e
Leah <lkramer37@nevada.unr.edu>
2020-03-04 20:34:43 -0800

    Refactored into separate atomic classes. Created all unit tests

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -3,99 +3,68 @@ import tic_tac_toe
 
 
 class MyTestCase(unittest.TestCase):
-    def test_space_marked(self):
-        board = tic_tac_toe.Board()
-
-        board.mark_square(0, 'X')
-        self.assertEqual(board.boardArr[0], "X")
-        board.mark_square(1, 'X')
-        self.assertEqual(board.boardArr[1], "X")
-        board.mark_square(2, 'X')
-        self.assertEqual(board.boardArr[2], "X")
-        board.mark_square(3, 'X')
-        self.assertEqual(board.boardArr[3], "X")
-        board.mark_square(4, 'X')
-        self.assertEqual(board.boardArr[4], "X")
-        board.mark_square(5, 'X')
-        self.assertEqual(board.boardArr[5], "X")
-        board.mark_square(6, 'X')
-        self.assertEqual(board.boardArr[6], "X")
-        board.mark_square(7, 'X')
-        self.assertEqual(board.boardArr[7], "X")
-        board.mark_square(8, 'X')
-        self.assertEqual(board.boardArr[8], "X")
-
-    def test_has_no_winner(self):
-        test_board = tic_tac_toe.Board()
-        player = 'X'
-        self.assertFalse(test_board.has_winner(player))
-
-    def test_convert_arr(self):
-        self.assertEqual(tic_tac_toe.convert_arr(1, 1), 0)
-        self.assertEqual(tic_tac_toe.convert_arr(1, 2), 1)
-        self.assertEqual(tic_tac_toe.convert_arr(1, 3), 2)
-        self.assertEqual(tic_tac_toe.convert_arr(2, 1), 3)
-        self.assertEqual(tic_tac_toe.convert_arr(2, 2), 4)
-        self.assertEqual(tic_tac_toe.convert_arr(2, 3), 5)
-        self.assertEqual(tic_tac_toe.convert_arr(3, 1), 6)
-        self.assertEqual(tic_tac_toe.convert_arr(3, 2), 7)
-        self.assertEqual(tic_tac_toe.convert_arr(3, 3), 8)
-
+    # Tests for Player Class
     def test_switch_player(self):
+        player = tic_tac_toe.Player()
+        player.current_player = 'X'
+        player.switch_player()
+
+        self.assertEqual(player.current_player, 'O')
+        player.switch_player()
+        self.assertEqual(player.current_player, 'X')
+
+    # Tests for Board Class
+    def test_print_board(self):
+        pass
+
+    def test_mark_square(self):
         board = tic_tac_toe.Board()
-        board.current_plyr = 'X'
-        board.switch_player()
-        self.assertEqual(board.current_plyr, 'O')
-        board.switch_player()
-        self.assertEqual(board.current_plyr, 'X')
+
+        board.index = 0
+        board.current_player = 'X'
+        board.mark_square()
+        self.assertEqual(board.boardArr[0], 'X')
+
+    def test_check_if_marked(self):
+        board = tic_tac_toe.Board()
+        board.index = 0
+        board.boardArr[0] = 'X'
+        self.assertTrue(board.check_if_marked())
+        board.index = 8
+        self.assertFalse(board.check_if_marked())
+
+    def test_check_if_full(self):
+        board = tic_tac_toe.Board()
+        board.print_board()
+        self.assertFalse(board.check_if_full())
+
+        for x in range(9):
+            board.boardArr[x] = '0'
+        self.assertTrue(board.check_if_full())
 
     def test_matching_row(self):
         board1 = tic_tac_toe.Board()
-        board1.boardArr[0] = 'X'
-        board1.boardArr[1] = 'X'
-        board1.boardArr[2] = 'X'
-        self.assertTrue(board1.matching_row('X'))
-
-        board2 = tic_tac_toe.Board()
-        board2.boardArr[3] = 'X'
-        board2.boardArr[4] = 'X'
-        board2.boardArr[5] = 'X'
-        self.assertTrue(board2.matching_row('X'))
-
-        board3 = tic_tac_toe.Board()
-        board3.boardArr[6] = 'X'
-        board3.boardArr[7] = 'X'
-        board3.boardArr[8] = 'X'
-        self.assertTrue(board3.matching_row('X'))
+        board1.boardArr[0] = '0'
+        board1.boardArr[1] = '0'
+        board1.boardArr[2] = '0'
+        self.assertTrue(board1.matching_row('0'))
 
         board_no_match = tic_tac_toe.Board()
         self.assertFalse(board_no_match.matching_row('X'))
         self.assertFalse(board_no_match.matching_row('O'))
 
-    def text_matching_col(self):
+    def test_matching_col(self):
         board1 = tic_tac_toe.Board()
         board1.boardArr[0] = 'X'
         board1.boardArr[3] = 'X'
         board1.boardArr[6] = 'X'
         self.assertTrue(board1.matching_col('X'))
 
-        board2 = tic_tac_toe.Board()
-        board2.boardArr[1] = 'X'
-        board2.boardArr[4] = 'X'
-        board2.boardArr[7] = 'X'
-        self.assertTrue(board2.matching_col('X'))
-
-        board3 = tic_tac_toe.Board()
-        board3.boardArr[2] = 'X'
-        board3.boardArr[5] = 'X'
-        board3.boardArr[8] = 'X'
-        self.assertTrue(board3.matching_col('X'))
-
         board_no_match = tic_tac_toe.Board()
         self.assertFalse(board_no_match.matching_col('X'))
         self.assertFalse(board_no_match.matching_col('O'))
 
-    def matching_diag(self):
+    def test_matching_diag(self):
         board1 = tic_tac_toe.Board()
         board1.boardArr[0] = 'X'
         board1.boardArr[4] = 'X'
@@ -112,23 +81,35 @@ class MyTestCase(unittest.TestCase):
         self.assertFalse(board_no_match.matching_diag('X'))
         self.assertFalse(board_no_match.matching_diag('O'))
 
-    def play_game(self):
-        board = tic_tac_toe.Board()
-        board.play_game(2, 2)
-        self.assertEqual(board.boardArr[4], board.current_plyr)
+    # Tests for Game Class
+    def test_get_board_index(self):
+        game = tic_tac_toe.Game()
+        game.get_board_index(2, 2)
+        self.assertEqual(game.index, 4)
 
-    def check_if_marked(self):
+    def test_get_player_move(self):
+        # Not testing I/O
         pass
 
-    def out_of_bounds(self):
+    def test_check_for_winner(self):
+        game = tic_tac_toe.Game()
+        player = 'X'
+        self.assertFalse(game.check_for_winner(player))
+
+    def test_play(self):
         pass
 
-    def board_full(self):
+    # Other Tests
+    def test_out_of_bounds(self):
         pass
 
-    #ARRANGE
-    #ACT
-    #ASSERT
+    def test_board_full(self):
+        pass
+
+    # ARRANGE
+    # ACT
+    # ASSERT
+
 
 if __name__ == '__main__':
     unittest.main()
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,35 +1,55 @@
-def convert_arr(row_str, col_str):
-    col = int(col_str)
-    row = int(row_str)
-    row -= 1
-    col -= 1
-    index = row * 3 + col
-    return index
+
+class Player(object):
+    def __init__(self):
+        self.player1 = 'X'
+        self.player2 = 'O'
+        self.current_player = self.player1
+
+    def switch_player(self):
+        if self.current_player == self.player1:
+            self.current_player = self.player2
+        else:
+            self.current_player = self.player1
 
 
-class Board(object):
+class Board(Player):
 
     def __init__(self):
-        self.boardArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        self.plyr1 = 'X'
-        self.plyr2 = 'O'
-        self.current_plyr = self.plyr2
+        super().__init__()
+        self.col = -1
+        self.row = -1
+        self.index = -1
+        self.boardArr = ['_', '_', '_', '_', '_', '_', '_', '_', '_']
 
     def print_board(self):
-        print("Printed Board")
+        print(" ")
         print(self.boardArr[0] + self.boardArr[1] + self.boardArr[2])
         print(self.boardArr[3] + self.boardArr[4] + self.boardArr[5])
         print(self.boardArr[6] + self.boardArr[7] + self.boardArr[8])
 
-    def mark_square(self, index, player):
+    def mark_square(self):
         #if(index>8):
-        self.boardArr[index] = player
+        self.boardArr[self.index] = self.current_player
 
-    def switch_player(self):
-        if self.current_plyr == self.plyr1:
-            self.current_plyr = self.plyr2
+    def check_if_marked(self):
+        if self.boardArr[self.index] == 'X' or self.boardArr[self.index] == 'O':
+            print("That spot is taken, try again player " + self.current_player)
+            return 1
         else:
-            self.current_plyr = self.plyr1
+            return 0
+
+    def check_if_full(self):
+        count = 0
+
+        for x in self.boardArr:
+            count += 1
+            if x == "_":
+                break
+
+        if count < 9:
+            return False
+        elif count == 9:
+            return True
 
     def matching_row(self, player):
         if self.boardArr[0] == player and self.boardArr[1] == player and self.boardArr[2] == player:
@@ -59,7 +79,24 @@ class Board(object):
         else:
             return 0
 
-    def has_winner(self, player):
+
+class Game(Board):
+    def __init__(self):
+        super().__init__()
+
+    def get_board_index(self, row_str, col_str):
+        self.col = int(col_str)
+        self.row = int(row_str)
+        self.row -= 1
+        self.col -= 1
+        self.index = self.row * 3 + self.col
+
+    def get_player_move(self):
+        print("Player " + game.current_player + "'s move")
+        row, col = input("Enter row, col: ").split(',')
+        self.get_board_index(row, col)
+
+    def check_for_winner(self, player):
         if self.matching_row(player):
             return 1
         elif self.matching_col(player):
@@ -69,19 +106,20 @@ class Board(object):
         else:
             return 0
 
-    def play_game(self, row, col):
-        index = convert_arr(row, col)
-        self.mark_square(index, self.current_plyr)
+    def play(self):
+        self.print_board()
+        self.switch_player()
+        self.get_player_move()
+        while self.check_if_marked():
+            self.get_player_move()
+        self.mark_square()
+        self.check_if_full()
 
 
 if __name__ == '__main__':
-    board = Board()
-    while not board.has_winner(board.current_plyr):
-        board.print_board()
-        board.switch_player()
-        print("Player " + board.current_plyr + "'s move")
-        row, col = input("Enter row, col: ").split(',')
-        board.play_game(row, col)
-
-    winner = board.current_plyr
+    game = Game()
+    while not game.check_for_winner(game.current_player):
+        game.play()
+    winner = game.current_player
     print("{} has won!".format(winner))
+    game.print_board()
